52bbd800979167063bbce448373faa6786e98a5a
Ryo1916 <telemon1916@gmail.com>
2019-02-16 17:03:45 +0900

    Set users routes for devise

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   root 'mains#top'
   get 'mains/top'
 
-  devise_for :users
+  devise_for :users, controllers: {
+    omniauth_callbacks: "users/omniauth_callbacks",
+    passwords: "users/passwords",
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
 end
