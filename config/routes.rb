d0365efa265a49b810dfa23b347716cb925cf52b
Ryo1916 <telemon1916@gmail.com>
2019-02-16 18:27:35 +0900

    Setup letter_opener_web

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
     registrations: 'users/registrations',
     sessions: 'users/sessions'
   }
+
+  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 end
