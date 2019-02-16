71deac710d5a5c785eead0a8642ae32272fa7247
Ryo1916 <telemon1916@gmail.com>
2019-02-16 16:44:01 +0900

    Generate User model by devise

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root 'mains#top'
   get 'mains/top'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
 end
