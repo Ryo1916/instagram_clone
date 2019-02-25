0bb978099a9aeef3bcb17f37a18f3a1cb8107d5b
Ryo1916 <telemon1916@gmail.com>
2019-02-25 18:23:54 +0900

    Add sample data because Faker::Avatar is not working

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,20 +4,26 @@ require 'openssl'
 # https://github.com/stympy/faker/issues/763
 OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
+def image_fetcher
+  open(Faker::Avatar.image(slug = nil, size = '300x300', format = 'jpg'))
+  rescue
+  open("https://robohash.org/sitsequiquia.png?size=300x300&set=set1")
+end
+
 puts 'Start inserting seed users...'
 
-User.create!(name:  "Example User",
+User.create!(name: "Example User",
              username: "exampleuser",
              email: "example@railstutorial.org",
              website: "https://railstutorial.jp",
              bio: Faker::Lorem.sentence(6),
              phone: 9910375368,
              gender: 1,
-             avatar: open(Faker::Avatar.image(slug = nil, size = '300x300', format = 'jpg')),
+             avatar: File.open("#{Rails.root}/spec/fixtures/download.jpeg"),
              password:              "password",
              password_confirmation: "password")
 
-99.times do |n|
+10.times do |n|
  user = User.create!({
     name: Faker::Name.name,
     username: Faker::Internet.unique.user_name,
@@ -26,7 +32,7 @@ User.create!(name:  "Example User",
     bio: Faker::Lorem.sentence(6),
     phone: Faker::PhoneNumber.cell_phone.to_i,
     gender: 0,
-    avatar: open(Faker::Avatar.image(slug = nil, size = '300x300', format = 'png')),
+    avatar: File.open("#{Rails.root}/spec/fixtures/avatar#{n+1}.png"),
     password:              "password",
     password_confirmation: "password"
   })
@@ -37,10 +43,10 @@ end
 puts "Start inserting seed posts..."
 
 users = User.order(:created_at).take(6)
-50.times do
+10.times do |n|
   users.each do |user|
     post = user.posts.create!({
-      image: open(Faker::Avatar.image(slug = nil, size = '300x300', format = 'jpg')),
+      image: File.open("#{Rails.root}/spec/fixtures/post#{n+1}.jpg"),
       content: Faker::Lorem.sentence(5)
     })
 
