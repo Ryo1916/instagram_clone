7d1dab0d26252023bdb32969167861c1decbcccf
Ryo1916 <telemon1916@gmail.com>
2019-03-01 14:42:02 +0900

    Set follow/unfollow button

--- config/routes.rb
+++ config/routes.rb
@@ -47,10 +47,10 @@ Rails.application.routes.draw do
     member do
       get :following, :followers
     end
+    resources :relationships, only: [:create, :destroy]
   end
   resources :posts do
     resources :likes, only: [:create, :destroy]
   end
-  resources :relationships, only: [:create, :destroy]
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 end
